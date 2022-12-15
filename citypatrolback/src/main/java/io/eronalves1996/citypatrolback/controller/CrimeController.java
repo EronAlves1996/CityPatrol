2feccc47c3741c0d1fedc2faa6993081fc750dfe
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 17:35:08 -0300

    fix: make getAllCrimesFromCity throw exception

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/CrimeController.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/CrimeController.java
@@ -1,16 +1,18 @@
 package io.eronalves1996.citypatrolback.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.eronalves1996.citypatrolback.model.City;
 import io.eronalves1996.citypatrolback.model.Crime;
-import io.eronalves1996.citypatrolback.model.Hood;
 import io.eronalves1996.citypatrolback.repository.CityRepository;
 import io.eronalves1996.citypatrolback.repository.CrimeRepository;
 import io.eronalves1996.citypatrolback.repository.HoodRepository;
@@ -41,13 +43,13 @@ public class CrimeController {
     public List<Crime> getAllCrimesFromCity(@PathParam("city") int cityId) {
         Optional<City> city = cityRepository.findById(cityId);
         if (city.isPresent()) {
-            return city.get().getHoods().stream().map(hood -> hood.getCrimes()).reduce(new List<Crime>(),
+            return city.get().getHoods().stream().map(hood -> hood.getCrimes()).reduce(new ArrayList<Crime>(),
                     (finalCrimeList, hoodCrimes) -> {
                         hoodCrimes.forEach(finalCrimeList::add);
                         return finalCrimeList;
                     });
         }
-
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "City doesn't exist");
     }
 
 }
