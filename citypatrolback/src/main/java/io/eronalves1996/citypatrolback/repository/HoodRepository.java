7507a4aedc0a4cb40d274b5757673c4aedb31de1
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 17:13:03 -0300

    feat: add delete by city and id in hoodRepository

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/repository/HoodRepository.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/repository/HoodRepository.java
@@ -12,4 +12,6 @@ public interface HoodRepository extends CrudRepository<Hood, Integer> {
     List<Hood> findByCity(City city);
 
     Hood findByCityAndId(City city, int id);
+
+    void deleteByCityAndId(City city, int id);
 }
