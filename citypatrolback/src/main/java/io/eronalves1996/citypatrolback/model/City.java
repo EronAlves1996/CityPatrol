25065038cce078321873bdd85524cd376af99845
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 00:54:56 -0300

    feat: make City a JPA Entity

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/model/City.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/model/City.java
@@ -1,7 +1,12 @@
 package io.eronalves1996.citypatrolback.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class City {
 
+    @Id
     private String id;
     private String name;
     private long populationNumber;
