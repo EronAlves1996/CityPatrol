16902103a5c8abf27f4089766ef75aa9b3833670
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 01:37:57 -0300

    feat: expose GET for hood

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/HoodController.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/HoodController.java
@@ -1,16 +1,40 @@
 package io.eronalves1996.citypatrolback.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import io.eronalves1996.citypatrolback.model.City;
+import io.eronalves1996.citypatrolback.model.Hood;
+import io.eronalves1996.citypatrolback.repository.CityRepository;
 import io.eronalves1996.citypatrolback.repository.HoodRepository;
 
+@RestController
+@RequestMapping("/city/{cityId}/hood")
 public class HoodController {
 
-    private HoodRepository repository;
+    private HoodRepository hoodRepository;
+    private CityRepository cityRepository;
 
     @Autowired
-    public HoodController(HoodRepository repository) {
-        this.repository = repository;
+    public HoodController(HoodRepository hoodRepository, CityRepository cityRepository) {
+        this.hoodRepository = hoodRepository;
+        this.cityRepository = cityRepository;
+    }
+
+    @GetMapping
+    public List<Hood> getHoodsForCity(@PathVariable("cityId") int cityId) {
+        Optional<City> city = cityRepository.findById(cityId);
+        if (city.isPresent())
+            return hoodRepository.findByCity(city.get());
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "City doesn't exist");
     }
 
 }
