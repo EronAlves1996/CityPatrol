e33d5603ceb65b077b563562688bf3b7e1393d6e
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 00:52:26 -0300

    feat: create City entity

---
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/model/City.java
@@ -0,0 +1,33 @@
+package io.eronalves1996.citypatrolback.model;
+
+public class City {
+
+    private String id;
+    private String name;
+    private long populationNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPopulationNumber() {
+        return populationNumber;
+    }
+
+    public void setPopulationNumber(long populationNumber) {
+        this.populationNumber = populationNumber;
+    }
+
+}
