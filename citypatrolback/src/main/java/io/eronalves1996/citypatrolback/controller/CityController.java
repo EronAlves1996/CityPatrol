40d99e0f53b87caf6d9e81e0e3e7476289f6b55e
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 01:18:59 -0300

    feat: expose get mappings for City

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/CityController.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/CityController.java
@@ -1,9 +1,16 @@
 package io.eronalves1996.citypatrolback.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import io.eronalves1996.citypatrolback.model.City;
 import io.eronalves1996.citypatrolback.repository.CityRepository;
 
 @RestController
@@ -15,6 +22,20 @@ public class CityController {
     @Autowired
     public CityController(CityRepository repository) {
         this.repository = repository;
+    }
+
+    @GetMapping
+    public Iterable<City> getCities() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public City getCityById(@PathVariable("id") int id) {
+        Optional<City> city = repository.findById(id);
+        if (city.isPresent()) {
+            return city.get();
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "City doesn't exist");
     }
 
 }
