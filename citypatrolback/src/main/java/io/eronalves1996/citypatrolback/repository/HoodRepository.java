dbc4ddb73a752898962eeb6923bc033eb62e157e
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 01:41:25 -0300

    feat: added findByCityAndId for Hood

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/repository/HoodRepository.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/repository/HoodRepository.java
@@ -10,4 +10,6 @@ import io.eronalves1996.citypatrolback.model.Hood;
 public interface HoodRepository extends CrudRepository<Hood, Integer> {
 
     List<Hood> findByCity(City city);
+
+    Hood findByCityAndId(City city, int id);
 }
