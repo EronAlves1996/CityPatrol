9ddbd9b6341cb32344d385d0dd012cfd37195dec
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 01:21:20 -0300

    feat: expose delete for city

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/CityController.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/CityController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,11 @@ public class CityController {
     @PutMapping
     public City updateCityInformation(City city) {
         return createCity(city);
+    }
+
+    @DeleteMapping
+    public void deleteCity(int id) {
+        repository.deleteById(id);
     }
 
 }
