68266985adbff64427b6db17f5a9cf03b523d300
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 17:33:25 -0300

    feat: create findByHoodIn in CrimeRepository

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/repository/CrimeRepository.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/repository/CrimeRepository.java
@@ -1,9 +1,13 @@
 package io.eronalves1996.citypatrolback.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import io.eronalves1996.citypatrolback.model.Crime;
+import io.eronalves1996.citypatrolback.model.Hood;
 
 public interface CrimeRepository extends CrudRepository<Crime, Integer> {
 
+    List<Crime> findByHoodIn(List<Hood> hoods);
 }
