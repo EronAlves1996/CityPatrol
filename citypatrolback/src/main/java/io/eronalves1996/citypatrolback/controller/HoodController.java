93a2ddb64ae86dbe533affc853f41992a7bf0dc5
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 01:40:45 -0300

    feat: expose another get mapping for hood

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/HoodController.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/HoodController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -36,5 +37,17 @@ public class HoodController {
             return hoodRepository.findByCity(city.get());
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "City doesn't exist");
     }
+
+    @GetMapping("/{id}")
+    public Hood getHood(@PathVariable("cityId") int cityId, @PathVariable("id") int id) {
+        Optional<City> city = cityRepository.findById(cityId);
+        if (city.isPresent())
+            return hoodRepository.findByCityAndId(city, id);
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "City or hood doesn't exist");
+
+    }
+
+    @PostMapping
+    public Hood c
 
 }
