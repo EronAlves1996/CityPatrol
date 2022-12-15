7c8d59099b66bf71fef457f4199c80c19e333cf3
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 01:04:17 -0300

    feat: create Crime entity

---
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/model/Crime.java
@@ -0,0 +1,50 @@
+package io.eronalves1996.citypatrolback.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Crime {
+
+    @Id
+    private int id;
+    private CrimeType type;
+    private String descrition;
+
+    @ManyToOne
+    private Hood hood;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CrimeType getType() {
+        return type;
+    }
+
+    public void setType(CrimeType type) {
+        this.type = type;
+    }
+
+    public String getDescrition() {
+        return descrition;
+    }
+
+    public void setDescrition(String descrition) {
+        this.descrition = descrition;
+    }
+
+    public Hood getHood() {
+        return hood;
+    }
+
+    public void setHood(Hood hood) {
+        this.hood = hood;
+    }
+
+}
