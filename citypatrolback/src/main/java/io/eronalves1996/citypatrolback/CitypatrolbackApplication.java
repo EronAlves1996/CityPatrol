5b2c4a0dff8e68e7af5a2d00f4a87caaa3010b99
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 00:48:17 -0300

    feat: initialized backend repo

---
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/CitypatrolbackApplication.java
@@ -0,0 +1,13 @@
+package io.eronalves1996.citypatrolback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitypatrolbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitypatrolbackApplication.class, args);
+	}
+
+}
---
+++ citypatrolback/src/test/java/io/eronalves1996/citypatrolback/CitypatrolbackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eronalves1996.citypatrolback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitypatrolbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
