daa27e5fdf135d1b6a73a7fae80e3d00ce6c9a14
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 20:43:53 -0300

    test: set context for test execution

--- citypatrolback/src/test/java/io/eronalves1996/citypatrolback/CitypatrolbackApplicationTests.java
+++ citypatrolback/src/test/java/io/eronalves1996/citypatrolback/CitypatrolbackApplicationTests.java
@@ -1,13 +1,35 @@
 package io.eronalves1996.citypatrolback;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CitypatrolbackApplicationTests {
 
+	private ExecutorService executor = Executors.newSingleThreadExecutor();
+	private Future<?> contextExecution;
+
+	@BeforeAll
+	public void loadContainer() {
+		executor.execute(() -> {
+			CitypatrolbackApplication.main(new String[0]);
+		});
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@AfterAll
+	public void unloadContainer() {
+		executor.shutdown();
+		executor.shutdownNow();
 	}
 
 }
