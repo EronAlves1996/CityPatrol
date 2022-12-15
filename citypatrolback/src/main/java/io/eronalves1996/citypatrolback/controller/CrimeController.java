1263a27337a08a59c79d9059b0ea16d510ee50fe
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 17:33:34 -0300

    feat: expose two get methods for crime

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/CrimeController.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/controller/CrimeController.java
@@ -1,20 +1,53 @@
 package io.eronalves1996.citypatrolback.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.eronalves1996.citypatrolback.model.City;
+import io.eronalves1996.citypatrolback.model.Crime;
+import io.eronalves1996.citypatrolback.model.Hood;
+import io.eronalves1996.citypatrolback.repository.CityRepository;
 import io.eronalves1996.citypatrolback.repository.CrimeRepository;
+import io.eronalves1996.citypatrolback.repository.HoodRepository;
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/crime")
 public class CrimeController {
 
-    private CrimeRepository repository;
+    private CrimeRepository crimeRepository;
+    private HoodRepository hoodRepository;
+    private CityRepository cityRepository;
 
     @Autowired
-    public CrimeController(CrimeRepository repository) {
-        this.repository = repository;
+    public CrimeController(CrimeRepository crimeRepository, HoodRepository hoodRepository,
+            CityRepository cityRepository) {
+        this.crimeRepository = crimeRepository;
+        this.hoodRepository = hoodRepository;
+        this.cityRepository = cityRepository;
+    }
+
+    @GetMapping
+    public Iterable<Crime> getAllCrimes() {
+        return crimeRepository.findAll();
+    }
+
+    @GetMapping
+    public List<Crime> getAllCrimesFromCity(@PathParam("city") int cityId) {
+        Optional<City> city = cityRepository.findById(cityId);
+        if (city.isPresent()) {
+            return city.get().getHoods().stream().map(hood -> hood.getCrimes()).reduce(new List<Crime>(),
+                    (finalCrimeList, hoodCrimes) -> {
+                        hoodCrimes.forEach(finalCrimeList::add);
+                        return finalCrimeList;
+                    });
+        }
+
     }
 
 }
