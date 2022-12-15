b38da03feb864bbecd262b185988777a1ba4c4d3
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 00:57:48 -0300

    feat: create a Region enum

---
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/model/Region.java
@@ -0,0 +1,14 @@
+package io.eronalves1996.citypatrolback.model;
+
+public enum Region {
+
+    NORTH,
+    NORTH_WEST,
+    WEST,
+    SOUTH_WEST,
+    SOUTH,
+    SOUTH_EAST,
+    EAST,
+    NORTH_EAST
+
+}
