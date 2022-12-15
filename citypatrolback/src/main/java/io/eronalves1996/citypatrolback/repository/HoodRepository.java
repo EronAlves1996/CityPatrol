61971ee03489f6e5335840e0d8c4fa45c8e16df1
Eron Alves <eron.a.silva@capgemini.com>
2022-12-15 01:33:50 -0300

    feat: add findByCity on HoodRepository

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/repository/HoodRepository.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/repository/HoodRepository.java
@@ -1,9 +1,13 @@
 package io.eronalves1996.citypatrolback.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import io.eronalves1996.citypatrolback.model.City;
 import io.eronalves1996.citypatrolback.model.Hood;
 
 public interface HoodRepository extends CrudRepository<Hood, Integer> {
 
+    List<Hood> findByCity(City city);
 }
