e8e2a23b9bacfc15d74877df746a4664a6bb55ff
Eron Alves <eron.a.silva@capgemini.com>
2022-12-16 00:06:50 -0300

    test: set HoodControllerTEst boilerplate code

---
+++ citypatrolback/src/test/java/io/eronalves1996/citypatrolback/HoodControllerTest.java
@@ -0,0 +1,115 @@
+package io.eronalves1996.citypatrolback;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import io.eronalves1996.citypatrolback.model.City;
+import io.eronalves1996.citypatrolback.repository.CityRepository;
+import io.eronalves1996.citypatrolback.repository.HoodRepository;
+
+@SpringBootTest
+class HoodControllerTest {
+
+	private static ExecutorService executor = Executors.newSingleThreadExecutor();
+	private RestTemplate restTemplate = new RestTemplate();
+
+	@Autowired
+	private HoodRepository hoodRepository;
+
+	@BeforeAll
+	public static void loadContainer() throws InterruptedException {
+		executor.execute(() -> {
+			CitypatrolbackApplication.main(new String[0]);
+		});
+		Thread.sleep(2000L);
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void testGetCities() {
+		Iterable<City> cities = cityRepository.findAll();
+		ResponseEntity<City[]> response = restTemplate.getForEntity("http://localhost:8080/city", City[].class);
+		City[] citiesFetched = response.getBody();
+		List<City> citiesTested = new ArrayList<>();
+		cities.forEach(citiesTested::add);
+
+		assertNotNull(citiesFetched);
+		assertEquals(citiesTested.size(), List.of(citiesFetched).size());
+	}
+
+	@Test
+	public void testGetCityById() {
+		Optional<City> testedCity = cityRepository.findById(1);
+		if (testedCity.isPresent()) {
+			int id = testedCity.get().getId();
+			ResponseEntity<City> cityFetched = restTemplate.getForEntity("http://localhost:8080/city/" + id,
+					City.class);
+			assertEquals(testedCity.get(), cityFetched.getBody());
+		}
+	}
+
+	@Test
+	public void testGetCityByAnIdThatNotExists() {
+		HttpStatusCode statusCode = assertThrows(HttpClientErrorException.class,
+				() -> restTemplate.getForEntity("http://localhost:8080/city/" + 30, City.class)).getStatusCode();
+		assertEquals(HttpStatusCode.valueOf(404), statusCode);
+	}
+
+	@Test
+	public void testPostCity() {
+		City city = new City();
+		city.setName("Divinópolis");
+		city.setPopulationNumber(20000);
+		ResponseEntity<City> cityCreated = restTemplate.postForEntity("http://localhost:8080/city", city, City.class);
+		assertNotNull(cityCreated.getBody().getId());
+	}
+
+	@Test
+	public void testPutCity() {
+		City city = restTemplate.getForEntity("http://localhost:8080/city/1", City.class).getBody();
+		city.setName("Josafá");
+		city.setHoods(null);
+		restTemplate.put("http://localhost:8080/city", city);
+		City city2 = restTemplate.getForEntity("http://localhost:8080/city/1", City.class).getBody();
+		assertNotNull(city.getName(), city2.getName());
+	}
+
+	@Test
+	public void testDeleteCity() {
+		ResponseEntity<City[]> response = restTemplate.getForEntity("http://localhost:8080/city",
+				City[].class);
+		List<City> cities = Arrays.asList(response.getBody());
+		int lastId = cities.get(cities.size() - 1).getId();
+		restTemplate.delete("http://localhost:8080/city/" + lastId);
+		assertThrows(HttpClientErrorException.class,
+				() -> restTemplate.getForEntity("http://localhost:8080/city/" + lastId, City.class));
+	}
+
+	@AfterAll
+	public static void unloadContainer() {
+		executor.shutdown();
+		executor.shutdownNow();
+	}
+
+}
