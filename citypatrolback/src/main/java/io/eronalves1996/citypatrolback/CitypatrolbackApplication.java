41e73899f938d55da426c84c201f3d99c89190c7
Eron Alves <eron.a.silva@capgemini.com>
2022-12-19 14:50:03 -0300

    docs: configure application for documentation

--- citypatrolback/src/main/java/io/eronalves1996/citypatrolback/CitypatrolbackApplication.java
+++ citypatrolback/src/main/java/io/eronalves1996/citypatrolback/CitypatrolbackApplication.java
@@ -3,7 +3,11 @@ package io.eronalves1996.citypatrolback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "City Patrol API", description = "Documentation for City Patrol API endpoints"))
 public class CitypatrolbackApplication {
 
 	public static void main(String[] args) {
